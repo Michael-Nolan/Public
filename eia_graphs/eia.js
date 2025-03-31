373d31607a190e7ac3a96ab2fb10771abd329174
Michael Nolan <mike.nolan.joshua@gmail.come>
2025-03-30 23:01:48 -0500

    Format

--- eia_graphs/eia.js
+++ eia_graphs/eia.js
@@ -1,223 +1,222 @@
 function processRawData() {
-    rawData.response.data.sort((a, b) => a.period.localeCompare(b.period));
+  rawData.response.data.sort((a, b) => a.period.localeCompare(b.period));
+
+  const groupedData = new Map();
+
+  rawData.response.data.forEach(item => {
+    const fuelType = item.fuelTypeDescription;
+    if (!groupedData.has(fuelType)) {
+      groupedData.set(fuelType, []);
+    }
+    groupedData.get(fuelType).push(item);
+  });
 
 
-    const groupedData = new Map(); 
+  // Transform and sort data for each fuel type
+  const result = new Map();
 
-    rawData.response.data.forEach(item => {
-        const fuelType = item.fuelTypeDescription;
-        if (!groupedData.has(fuelType)) {
-            groupedData.set(fuelType, []);
-        }
-        groupedData.get(fuelType).push(item);
+  groupedData.forEach((value, key) => {
+    // Sort data by period in descending order
+    groupedData.get(key).sort((a, b) => a.period.localeCompare(b.period));
+
+    // Extract dates and values
+    const dates = groupedData.get(key).map(item => item.period);
+    const values = groupedData.get(key).map(item => parseFloat(item.generation));
+
+    // Assign to result
+    result.set(key, {
+      x: dates,
+      y: values,
+      name: key,
     });
+  });
+
+  return result
+}
+
+function plotNetGenMonthly(data) {
+  var dd = [];
+  data.forEach((value, key) => {
+    value["type"] = "scatter"
+    dd.push(value)
+  });
+
+  const myplot = document.getElementById('plotNetGenMonthly');
+  var layout = {
+    title: {
+      text: 'Net Generation by Source'
+    },
+    xaxis: {
+      title: {
+        text: 'month'
+      },
+      fixedrange: false  // Allow zooming on x-axis
+    },
+    yaxis: {
+      title: {
+        text: '1,000 mwh',
+      },
+      fixedrange: true  // Prevent direct y-axis zooming
+    }
+  };
+
+  var config = {
+    toImageButtonOptions: {
+      format: 'svg', // one of png, svg, jpeg, webp
+      filename: 'plotNetGenMonthly',
+      scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
+    },
+    scrollZoom: true
+  };
+
+  Plotly.newPlot('plotNetGenMonthly', dd, layout, config);
+}
+
+function plotPercentGenMonthly(data) {
+  var dd = [];
+  data.forEach((value, key) => {
+    value["type"] = "scatter"
+    value["groupnorm"] = "percent"
+    value["stackgroup"] = "one"
+    dd.push(value)
+  });
+
+  var layout = {
+    title: {
+      text: 'Percent of Generation by Source'
+    },
+    xaxis: {
+      title: {
+        text: 'month'
+      }
+    },
+    yaxis: {
+      title: {
+        text: 'Percent'
+      },
+      fixedrange: true,
+    }
+  };
+
+  var config = {
+    toImageButtonOptions: {
+      format: 'svg', // one of png, svg, jpeg, webp
+      filename: 'plotPercentGenMonthly',
+      scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
+    },
+    scrollZoom: true
+  };
+
+  Plotly.newPlot('plotPercentGenMonthly', dd, layout, config);
+}
 
 
-    // Transform and sort data for each fuel type
-    const result = new Map(); 
+function convertToRolling(data, windowSize) {
+  const result = new Map();
 
-    groupedData.forEach((value, key) => {
-        // Sort data by period in descending order
-        groupedData.get(key).sort((a, b) => a.period.localeCompare(b.period));
+  data.forEach((value, key) => {
+    newX = [];
+    newY = [];
 
-        // Extract dates and values
-        const dates = groupedData.get(key).map(item => item.period);
-        const values = groupedData.get(key).map(item => parseFloat(item.generation));
+    var sum = 0;
+    for (let i = 0; i < value.x.length; i++) {
+      sum += value.y[i];
 
-        // Assign to result
-        result.set(key, {
-            x:dates,
-            y:values,
-            name: key,
-        });
+      if (i < windowSize - 1) {
+        continue;
+      }
+
+      if (i > windowSize - 1) {
+        sum -= value.y[i - windowSize];
+      }
+
+      newY.push(sum);
+      newX.push(value.x[i]);
+    }
+
+    // Assign to result
+    result.set(key, {
+      x: newX,
+      y: newY,
+      name: key,
     });
+  });
 
-    return result
+  return result;
 }
 
-function plotNetGenMonthly(data){
-    var dd = [];
-    data.forEach((value, key) => {
-        value["type"] = "scatter"
-        dd.push(value)
-    }); 
+function plotNetGenRolling(data) {
+  var dd = [];
+  data.forEach((value, key) => {
+    value["type"] = "scatter"
+    dd.push(value)
+  });
 
-    const myplot = document.getElementById('plotNetGenMonthly');
-    var layout = {
-        title: {
-          text: 'Net Generation by Source'
-        },
-        xaxis: {
-          title: {
-            text: 'month'
-          },
-          fixedrange: false  // Allow zooming on x-axis
-        },
-        yaxis: {
-          title: {
-            text: '1,000 mwh',
-          },
-          fixedrange: true  // Prevent direct y-axis zooming
-        }
-      };
+  const myplot = document.getElementById('plotNetGenRolling');
+  var layout = {
+    title: {
+      text: 'Net Generation by Source (12 Month Rolling)'
+    },
+    xaxis: {
+      title: {
+        text: 'month'
+      },
+      fixedrange: false  // Allow zooming on x-axis
+    },
+    yaxis: {
+      title: {
+        text: '1,000 mwh',
+      },
+      fixedrange: true  // Prevent direct y-axis zooming
+    }
+  };
 
-      var config = {
-        toImageButtonOptions: {
-          format: 'svg', // one of png, svg, jpeg, webp
-          filename: 'plotNetGenMonthly',
-          scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
-        },
-        scrollZoom: true
-      };
+  var config = {
+    toImageButtonOptions: {
+      format: 'svg', // one of png, svg, jpeg, webp
+      filename: 'plotNetGenRolling',
+      scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
+    },
+    scrollZoom: true
+  };
 
-    Plotly.newPlot('plotNetGenMonthly', dd, layout, config);
+  Plotly.newPlot('plotNetGenRolling', dd, layout, config);
 }
 
-function plotPercentGenMonthly(data){
-    var dd = [];
-    data.forEach((value, key) => {
-        value["type"] = "scatter"
-        value["groupnorm"] = "percent"
-        value["stackgroup"] = "one"
-        dd.push(value)
-    }); 
+function plotPercentGenRolling(data) {
+  var dd = [];
+  data.forEach((value, key) => {
+    value["type"] = "scatter"
+    value["groupnorm"] = "percent"
+    value["stackgroup"] = "one"
+    dd.push(value)
+  });
 
-    var layout = {
-        title: {
-          text: 'Percent of Generation by Source'
-        },
-        xaxis: {
-          title: {
-            text: 'month'
-          }
-        },
-        yaxis: {
-          title: {
-            text: 'Percent'
-          },
-          fixedrange: true,
-        }
-      };
+  var layout = {
+    title: {
+      text: 'Percent of Generation by Source (12 Month Rolling)'
+    },
+    xaxis: {
+      title: {
+        text: 'month'
+      }
+    },
+    yaxis: {
+      title: {
+        text: 'Percent'
+      },
+      fixedrange: true,
+    }
+  };
 
-      var config = {
-        toImageButtonOptions: {
-          format: 'svg', // one of png, svg, jpeg, webp
-          filename: 'plotPercentGenMonthly',
-          scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
-        },
-        scrollZoom: true
-      };
+  var config = {
+    toImageButtonOptions: {
+      format: 'svg', // one of png, svg, jpeg, webp
+      filename: 'plotPercentGenRolling',
+      scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
+    },
+    scrollZoom: true
+  };
 
-    Plotly.newPlot('plotPercentGenMonthly', dd, layout, config);
-}
-
-
-function convertToRolling(data, windowSize){
-    const result = new Map(); 
-
-    data.forEach((value, key) => {
-        newX = [];
-        newY = [];
-
-        var sum = 0;
-        for (let i = 0; i < value.x.length; i++) {
-           sum += value.y[i];
-
-            if (i < windowSize-1){
-                continue;
-            }
-            
-            if (i > windowSize-1) {
-                sum -= value.y[i-windowSize];
-            }
-            
-            newY.push(sum);
-            newX.push(value.x[i]);
-        }
-
-        // Assign to result
-        result.set(key, {
-            x:newX,
-            y:newY,
-            name: key,
-        });
-    });
-
-    return result;
-}
-
-function plotNetGenRolling(data){
-    var dd = [];
-    data.forEach((value, key) => {
-        value["type"] = "scatter"
-        dd.push(value)
-    }); 
-
-    const myplot = document.getElementById('plotNetGenRolling');
-    var layout = {
-        title: {
-          text: 'Net Generation by Source (12 Month Rolling)'
-        },
-        xaxis: {
-          title: {
-            text: 'month'
-          },
-          fixedrange: false  // Allow zooming on x-axis
-        },
-        yaxis: {
-          title: {
-            text: '1,000 mwh',
-          },
-          fixedrange: true  // Prevent direct y-axis zooming
-        }
-      };
-
-      var config = {
-        toImageButtonOptions: {
-          format: 'svg', // one of png, svg, jpeg, webp
-          filename: 'plotNetGenRolling',
-          scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
-        },
-        scrollZoom: true
-      };
-
-    Plotly.newPlot('plotNetGenRolling', dd, layout, config);
-}
-
-function plotPercentGenRolling(data){
-    var dd = [];
-    data.forEach((value, key) => {
-        value["type"] = "scatter"
-        value["groupnorm"] = "percent"
-        value["stackgroup"] = "one"
-        dd.push(value)
-    }); 
-
-    var layout = {
-        title: {
-          text: 'Percent of Generation by Source (12 Month Rolling)'
-        },
-        xaxis: {
-          title: {
-            text: 'month'
-          }
-        },
-        yaxis: {
-          title: {
-            text: 'Percent'
-          },
-          fixedrange: true,
-        }
-      };
-
-      var config = {
-        toImageButtonOptions: {
-          format: 'svg', // one of png, svg, jpeg, webp
-          filename: 'plotPercentGenRolling',
-          scale: 1 // Multiply title/legend/axis/canvas sizes by this factor
-        },
-        scrollZoom: true
-      };
-
-    Plotly.newPlot('plotPercentGenRolling', dd, layout, config);
+  Plotly.newPlot('plotPercentGenRolling', dd, layout, config);
 }
